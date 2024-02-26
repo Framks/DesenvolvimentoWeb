6ba196adfb8468b1cc42fd610669d24c591b5c48
Francisco Gabriel <galvesa9@gmail.com>
2024-02-26 11:40:12 -0300

    aula do dia 26-02

---
+++ Aula 01/introducao/src/components/01HelloWorld/HelloWorld.js
@@ -0,0 +1,25 @@
+// fomra classica 
+/*function HelloWold(){
+    return (
+        <div>
+            <h2>Meu primeiro commponente REACT!</h2>
+        </div>
+    )
+}*/
+
+// arrow fnctions
+/*const HelloWold = () => {
+    return (
+        <div>
+            <h2>Funão ARROW</h2>
+        </div>
+    )
+}*/
+
+const HelloWold = () => 
+    <div>
+        <h2>Funão ARROW</h2>
+    </div>
+
+
+export default HelloWold
